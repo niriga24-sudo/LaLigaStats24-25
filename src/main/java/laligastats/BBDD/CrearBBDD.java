59e54c2911582b4960799b829c8d59df3ceddc81
Nico Rios <niriga24@bemen3.cat>
2025-12-04 18:51:20 +0100

    correcion crearbbdd ahora si

--- src/main/java/laligastats/BBDD/CrearBBDD.java
+++ src/main/java/laligastats/BBDD/CrearBBDD.java
@@ -4,8 +4,8 @@ import laligastats.Connexio;
 
 public class CrearBBDD {
     public static void main(String[] args) {
-       Boolean BBDDCreada = Connexio.deleteBBDD();
+       Boolean BBDDCreada = Connexio.crearBBDD();
        System.out.println(BBDDCreada);
     }
-    
+
 }
