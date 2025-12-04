cab382b276ba5c8d4d428aafa1068458afcd0c14
Nico Rios <niriga24@bemen3.cat>
2025-12-04 18:35:52 +0100

    classe connexio i funcions crear i eliminar bbdd

--- src/main/java/laligastats/Connexio.java
+++ src/main/java/laligastats/Connexio.java
@@ -1,7 +1,39 @@
 package laligastats;
+import java.sql.*;
 
 public class Connexio {
 
     public static final String URL = "jdbc:mysql://localhost:3306/";
-    private 
+    public static final String USER =  "root";
+    public static final String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public boolean crearBBDD() {
+        try (Connection con = Connexio.getConnection()) {
+            String sql = "CREATE DATABASE LaLiga_23_24_Stats";
+            PreparedStatement ps = con.prepareStatement(sql);
+            System.out.println("BBDD LaLiga_23_24_Stats creada");
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean deleteBBDD() {
+        try (Connection con = Connexio.getConnection()) {
+            String sql = "DROP IF EXISTS LaLiga_23_24_Stats";
+            PreparedStatement ps = con.prepareStatement(sql);
+            System.out.println("BBDD LaLiga_23_24_Stats eliminada");
+            
+
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
